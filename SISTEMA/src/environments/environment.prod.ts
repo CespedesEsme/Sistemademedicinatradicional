d1ac145ff5bd26a9abcd83b21a0feafe1774c8c9
esmeralda <cespedes.esmeralda.126@gmail.com>
2022-10-17 23:04:42 -0400

    cambios guardados listos para la revision del 3er reled en progra

--- SISTEMA/src/environments/environment.prod.ts
+++ SISTEMA/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   env: 'PROD',
   production: true,
-   serverBaseUrl: 'http://bkcajasistem.vaixs.org/public/api',
+  // serverBaseUrl: 'http://bkcajasistem.vaixs.org/public/api', 
   serverBaseUrl: 'http://localhost/bkunimetra/public/api',
   serverUrl: 'http://localhost/bkunimetra/public/api',
   imgUrl: 'http://localhost/bkunimetra/public/'
